47a2868a82d19c7ceb8c75a0aa72b58296c310dc
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2023-12-29 21:32:34 +0600

    new commit

--- java_basic_java/addition_scanner_Class2.java
+++ java_basic_java/addition_scanner_Class2.java
@@ -2,22 +2,22 @@ package java_basic_java;
 
 import java.util.Scanner;
 
-public class addition_scanner_Class {
+public class addition_scanner_Class2 {
 
 	public static void main(String[] args) {
 		// TODO The Scanner class provides the methods that allows us to read the user input.
 		
+		int num1, num2, sum;
 		
 		Scanner addscanner = new Scanner(System.in);
 
 		System.out.println("Enter 1st number: ");
-		int num1 = addscanner.nextInt();
+		num1 = addscanner.nextInt();
 		
 		System.out.println("Enter 2nd number: ");
-		int num2 = addscanner.nextInt();
+		num2 = addscanner.nextInt();
 		
-		int sum = num1 + num2;
-		
+		sum = num1 + num2;
 		System.out.println("The sum is : "+sum);
 	}
 
