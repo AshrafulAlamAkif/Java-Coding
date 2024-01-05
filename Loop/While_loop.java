ab067debb6a24dce404e2e2a5922055a884ad4fe
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-01-05 21:17:45 +0600

    simple while loop

---
+++ Loop/While_loop.java
@@ -0,0 +1,16 @@
+package Loop;
+
+public class While_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 0;
+		while(i<10) {
+			System.out.println("the number is : "+i);
+			i++;
+		}
+
+	}
+
+}
