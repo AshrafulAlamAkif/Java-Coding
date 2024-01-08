2ed3406d5879cdebbf5c6d2e123967438d2a7156
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-01-08 23:25:04 +0600

    Continue and If Statement using in For loop

--- Loop/ForLoop_if$Continue.java
+++ Loop/ForLoop_if$Continue.java
@@ -9,9 +9,9 @@ public class ForLoop_if$Continue {
 		for(int i = 1; i<=9; i++) {
 			// Check if i is equal to 3
 			if(i ==3) {
-				// If i is 3, print a message and increment i, then continue to the next iteration
-				System.out.println("Desire Number: "+i);
-				i++;
+				// If i is 3, print a message and /*increment i,*/ then continue to the next iteration
+				System.out.println("Our Worst Number: "+i);
+				//i++;
 				continue;
 			}
 			// If i is not 3, print a different message
