a04c350e5fa8f352a33ed91040608a19d02b9c5e
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-01-11 23:12:19 +0600

    Nested for loop(Outer & inner loop)

--- Loop/Nested_For_Loop.java
+++ Loop/Nested_For_Loop.java
@@ -5,9 +5,11 @@ public class Nested_For_Loop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Outer loop
 		for(int i = 0; i <=2; i++) {
 			System.out.println("Outer Result: "+i);
 			
+			//Inner loop
 			for(int j = 0; j<=3; j++) {
 				System.out.println("Inner Result is: "+j);
 			}
