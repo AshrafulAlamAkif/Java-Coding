bbd74542b504cc3fb63cda2dc4c3e73af945fc08
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-06-05 00:13:55 +0600

    Print same sentence and count character

--- Problem_Solve/Get_specific_char_with_num2.java
+++ Problem_Solve/Get_specific_char_with_num2.java
@@ -12,27 +12,29 @@ public class Get_specific_char_with_num2 {
 		 */
 		
 		// Create a Scanner object named user
-		Scanner user = new Scanner(System.in);
-		
-		// Prompt the user for input
-		System.out.println("Please Enter any sentence: ");
-		String userName = user.nextLine();
-		
-		System.out.println("Now Enter your alphabet: ");
-		char userChar = user.nextLine().charAt(0);// Get the first character of the input
-		
-		// Close the Scanner to release resources
-		user.close();
-		
-		int i;
-		int count = 0;
-		
-		for(i = 0; i<userName.length(); i++) {
-			if(userName.charAt(i) == userChar) {
-				count++;
-			}
-		}
-		System.out.println("Your sentence is: "+userName);
-		System.out.println("The '"+userChar+"' Character has come "+count+ " times");
-	}
+Scanner user = new Scanner(System.in);
+        
+        // Prompt the user for input
+        System.out.println("Please Enter any sentence: ");
+        String userName = user.nextLine();  // Removed toLowerCase()
+        //String userName = user.nextLine().toLowerCase();
+        
+        System.out.println("Now Enter your alphabet: ");
+        char userChar = user.nextLine().charAt(0);  // Removed toLowerCase()
+        //char userChar = Character.toLowerCase(user.nextLine().charAt(0));// Get the first character of the input
+        
+        // Close the Scanner to release resources
+        user.close();
+        
+        int count = 0;
+        
+        for(int i = 0; i < userName.length(); i++) {
+            //if(Character.toLowerCase(userName.charAt(i)) == Character.toLowerCase(userChar)) {
+            	if(userName.charAt(i) == userChar) {
+                count++;
+            }
+        }
+        System.out.println("Your sentence is: " + userName);
+        System.out.println("The '" + userChar + "' Character has come " + count + " times");
+    }
 }
